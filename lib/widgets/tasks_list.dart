ecb45fb74f25dc530beea272bdd6be5bb6d0b107
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-02-14 19:41:32 +0900

    Consumerを使用する

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -5,22 +5,30 @@ import 'package:provider/provider.dart';
 
 // タスクのリスト一覧
 class TasksList extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        return TaskTile(
-          isChecked: Provider.of<TaskData>(context).tasks[index].isDone,
-          taskTitle: Provider.of<TaskData>(context).tasks[index].name,
-          checkboxCallback: (checkboxState) {
-            // setState(() {
-            //   widget.tasks[index].toggleDone();
-            // });
+    // notifyされた際、再ビルドの対象を絞るためのWidget
+    // TaskDataインスタンスが更新されるたびにbuilder関数が実行される
+    return Consumer<TaskData>(
+      // Widget Function(BuildContext, dynamic, Widget)
+      // 第2引数：TaskDataインスタンス
+      // 第3引数：Consumerコンストラクタにchildとして渡したインスタンス
+      builder: (context, taskData, child) {
+        return ListView.builder(
+          itemBuilder: (context, index) {
+            return TaskTile(
+              isChecked: taskData.tasks[index].isDone,
+              taskTitle: taskData.tasks[index].name,
+              checkboxCallback: (checkboxState) {
+                // setState(() {
+                //   widget.tasks[index].toggleDone();
+                // });
+              },
+            );
           },
+          itemCount: taskData.tasks.length,
         );
       },
-      itemCount: Provider.of<TaskData>(context).tasks.length,
     );
   }
 }
