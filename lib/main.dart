cee86c37f1aaf86c26be6072c8b25e106711e667
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-02-14 18:59:11 +0900

    ChangeNotifierProviderを使用

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'models/task_data.dart';
 import 'package:todoey_practice/screens/tasks_screen.dart';
 
 void main() => runApp(MyApp());
@@ -7,8 +9,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: TasksScreen(),
+    // データが変わったことを知りWidgetを作り直す(ChangeNotifierProvider)を使って、状態管理を行う
+    return ChangeNotifierProvider(
+      create: (context) => TaskData(),
+      child: MaterialApp(
+        home: TasksScreen(),
+      ),
     );
   }
 }
