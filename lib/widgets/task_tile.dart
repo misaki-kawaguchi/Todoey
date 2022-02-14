95d70cbc48826b1cd7a37141c4eb43fde6c30af6
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-02-14 16:30:13 +0900

    TasksScreenをStateLesswidgetにする

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -7,10 +7,25 @@ class TaskTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       title: Text('This is a task.'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (bool? value) {},
-      ),
+      trailing: TaskCheckbox(),
+    );
+  }
+}
+
+// チェックボックス
+class TaskCheckbox extends StatefulWidget {
+
+  @override
+  State<TaskCheckbox> createState() => _TaskCheckboxState();
+}
+
+class _TaskCheckboxState extends State<TaskCheckbox> {
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: false,
+      onChanged: (bool? value) {},
     );
   }
 }
