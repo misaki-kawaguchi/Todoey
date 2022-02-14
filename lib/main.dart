ed4234636c719a74aa541c2d28bf018a734e17af
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-02-14 13:59:23 +0900

    tasks_screens.dart作成

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todoey_practice/screens/tasks_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,6 +7,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: TasksScreen(),
+    );
   }
 }
