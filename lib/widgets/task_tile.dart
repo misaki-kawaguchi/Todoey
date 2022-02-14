40bb18568a3be9791c40c60a5f85957b4d7f5767
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-02-14 16:32:35 +0900

    isCheckedを使用できるようにする

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -20,12 +20,19 @@ class TaskCheckbox extends StatefulWidget {
 }
 
 class _TaskCheckboxState extends State<TaskCheckbox> {
+
+  bool isChecked = false;
+
   @override
   Widget build(BuildContext context) {
     return Checkbox(
       activeColor: Colors.lightBlueAccent,
-      value: false,
-      onChanged: (bool? value) {},
+      value: isChecked,
+      onChanged: (newValue) {
+        setState(() {
+          isChecked = newValue!;
+        });
+      },
     );
   }
 }
