7a799a1726b766803394a3c080ea7a32363c117b
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-02-14 16:48:22 +0900

    isCheckedがtrueの場合は横線を引く

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -2,27 +2,31 @@ import 'package:flutter/material.dart';
 
 // リスト1つ分
 class TaskTile extends StatefulWidget {
-
   @override
   State<TaskTile> createState() => _TaskTileState();
 }
 
 class _TaskTileState extends State<TaskTile> {
-
   bool isChecked = false;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is a task.'),
-      trailing: TaskCheckbox(checkboxState: isChecked),
+      title: Text(
+        'This is a task.',
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
+      ),
+      trailing: TaskCheckbox(
+        checkboxState: isChecked,
+      ),
     );
   }
 }
 
 // チェックボックス
 class TaskCheckbox extends StatelessWidget {
-
   final bool checkboxState;
   TaskCheckbox({required this.checkboxState});
 
@@ -31,8 +35,7 @@ class TaskCheckbox extends StatelessWidget {
     return Checkbox(
       activeColor: Colors.lightBlueAccent,
       value: checkboxState,
-      onChanged: (newValue) {
-      },
+      onChanged: (newValue) {},
     );
   }
 }
